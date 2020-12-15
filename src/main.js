49ad3b8dbe14d453f524d0dfbd3418e8d34b0a4c
zhangjinke1164123394 <56053424+zhangjinke1164123394@users.noreply.github.com>
2020-12-15 09:22:15 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import 'element-ui/lib/theme-chalk/index.css';
 import router from  "./routes"
 import VueRouter from 'vue-router'
 import store from './store'
+import imgLazy from './imgLizy'
 Vue.use(VueRouter)
 Vue.use(ElementUI)
-
+Vue.mixin({
+  mounted(){
+    imgLazy.change()
+  }
+})
 Vue.config.productionTip = false
 
 new Vue({
